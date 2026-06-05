8a3d10a3032717d6aecd314681930dc2685d27f4
Arvi <virzuzz12345@gmail.com>
2026-06-05 15:13:11 -0400

    fix de back otra vez con rebase

--- src/routes/juego.routes.ts
+++ src/routes/juego.routes.ts
@@ -17,6 +17,13 @@ router.get(
   Autenticacion.autenticarToken,
   GameController.getGameById
 );
+router.get('/', Autorizacion.autenticarToken, juegoController.getAllGames);
+
+router.get('/:id', Autorizacion.autenticarToken, GameController.getGameById);
+
+router.get('/servicios', Autorizacion.autenticarToken, GameController.getGameServices);
+
+router.post('/', Autorizacion.autenticarToken, GameController.createGame);
 
 router.get(
   "/sistema/recientes",
